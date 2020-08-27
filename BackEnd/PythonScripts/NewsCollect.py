e7333daf13d7b0dfeb43cb214d4500017b8eecfc
LEEJAEBEEN <woqls226@gmail.com>
2020-08-27 16:14:12 +0900

    데이터셋 셔플모듈 추가

--- BackEnd/PythonScripts/NewsCollect.py
+++ BackEnd/PythonScripts/NewsCollect.py
@@ -1,12 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
-
+import random
+import pandas as pd
 def get_newses(keyword):
     f = open("navernews.csv", "w")
     # 데이터의 헤더부분을 입력한다.
-    f.write("제목, 언론사\n")
+    f.write("headline, label\n")
+    number = 0
     page = 1
-    for page in range(1, 4000,10):
+    for page in range(1, 1000,10):
         raw = requests.get("https://search.naver.com/search.naver?where=news&query="+keyword+"&start=" + str(page),
                            headers={"User-Agent": "Mozilla/5.0"})
         html = BeautifulSoup(raw.text, 'html.parser')
@@ -17,8 +19,8 @@ def get_newses(keyword):
         for ar in articles:
             title = ar.select_one("a._sp_each_title").text
             source = ar.select_one("span._sp_each_source").text
-
-            print(title)
+            number=number+1
+            print('['+str(number)+']'+title)
 
             title = title.replace(",", "")
             source = source.replace(",", "")
@@ -28,5 +30,25 @@ def get_newses(keyword):
                 pass
     # navernews.csv 파일 닫음
     f.close()
+
+def shuffle(filename):
+    data = pd.read_csv(filename, encoding='CP949')
+    x_train = []
+    y_train = []
+    for sentence in data['headline']:
+        x_train.append(sentence)
+    for sentence in data['label']:
+        y_train.append(sentence)
+    tmp = [[x, y] for x, y in zip(x_train, y_train)]
+    random.shuffle(tmp)
+    x_train = [n[0] for n in tmp]
+    y_train = [n[1] for n in tmp]
+    data = pd.DataFrame({
+        'headline': x_train,
+        'label': y_train
+    })
+    data.to_csv(filename, index=False, encoding='cp949')
+
 if __name__ == '__main__':
-    get_newses("실적개선상한가")
+    #get_newses("업계 절규")
+    shuffle('Data/test.csv')
