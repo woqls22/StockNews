82ab908e82e4074dcc198f68882d3dff90b59124
LEEJAEBEEN <woqls226@gmail.com>
2020-09-07 00:28:31 +0900

    종목별 가격정보 DB저장기능 구현

--- BackEnd/PythonScripts/DBHandler.py
+++ BackEnd/PythonScripts/DBHandler.py
@@ -3,6 +3,8 @@
 # Data/News ~ .csv
 # Data/CompanyNewsList.csv
 import pymysql
+import xlrd
+import pandas as pd
 
 class MySqlController:
     def __init__(self, host, id, pw, db_name):
@@ -20,3 +22,34 @@ class MySqlController:
             self.curs.execute(sql, (CompanyFromNews[index], Headline[index], Text[index], URL[index], NewsInfo[index], DateTime))
             self.conn.commit()
         print("Stock NewsHistory Updated.")
+
+    def UpdatePrice(self):
+        xlsx = xlrd.open_workbook('Data/stockdata.xls')
+        sheet = xlsx.sheet_by_index(0)
+        for i in range(1, 101):
+            code = sheet.cell(i, 0).value
+            stock = sheet.cell(i, 1).value
+            for j in range(1, 50):
+                info_csv = pd.read_csv('Data/Histories/' + stock + '_info.csv', encoding='CP949')
+                date = info_csv.iloc[j][0]
+                end_price = info_csv.iloc[j][1]
+                start_price = info_csv.iloc[j][2]
+                highest = info_csv.iloc[j][3]
+                lowest = info_csv.iloc[j][4]
+                volume = info_csv.iloc[j][5]
+                sql = "INSERT INTO info_"+code+" VALUES(%s, %s, %s, %s, %s, %s)"
+                print(sql)
+                print(date, end_price, start_price, highest, lowest,volume)
+                print()
+                self.curs.execute(sql, (date, end_price, start_price, highest, lowest,volume))
+                self.conn.commit()
+
+    def CreateTable(self):
+        xlsx = xlrd.open_workbook('Data/stockdata.xls')
+        sheet = xlsx.sheet_by_index(0)
+        for i in range(1, 101):
+            code = sheet.cell(i, 0).value
+            stock = sheet.cell(i, 1).value
+            sql = 'CREATE TABLE info_'+code+' (DATE_INFO DATE NOT NULL, END_PRICE VARCHAR(20) NOT NULL, START_PRICE VARCHAR(20) NOT NULL, HIGHEST VARCHAR(20) NOT NULL,LOWEST VARCHAR(20) NOT NULL, VOLUME VARCHAR(30));'
+            self.curs.execute(sql)
+            self.conn.commit()
