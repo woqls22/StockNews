4f60a45d6f6cf3148638773b44c958bbf38f75f3
LEEJAEBEEN <woqls226@gmail.com>
2020-09-14 01:55:31 +0900

    기능별 예외처리 적용

--- BackEnd/PythonScripts/main.py
+++ BackEnd/PythonScripts/main.py
@@ -12,6 +12,7 @@ ID= '계정명'
 PW='비밀번호'
 DB_name='DB이름'
 
+
 def GetNewsInfo(driver):
     headlines, news_info, Text,NewsUrl = Util.GetNews(driver) # 헤드라인, 신문사 정보 및 게시 시간, 본문, 기사링크 파싱.
     CompanyFromNews = Util.GetCompanyFromNews(headlines, CompanyList)
@@ -21,7 +22,7 @@ def GetNewsInfo(driver):
 
 def GetPriceInfo(driver):
     NameList, PriceInfo, Fluctuation = Util.get_prices(driver) #KTOP 30, KOSPI, KOSPI200, KOSDAQ, KOSDAQ150, KRX300 순
-    #Util.PrintPrice(NameList, PriceInfo, Fluctuation)
+    Util.PrintPrice(NameList, PriceInfo, Fluctuation)
     return NameList, PriceInfo, Fluctuation
 
 def MakeCompanyFile(MakeCompanyList):
@@ -33,56 +34,71 @@ if __name__ == '__main__':
     CompanyList = Util.GetCompanyList() # 코스피 상장 기업 업로드
     try: #네이버 증권
         NewsDriver = Util.News_get_driver(Headless)
-    except:
+    except Exception as ex:
         print("News Driver Err")
-    try: #한국거래소
+        print('에러가 발생 했습니다', ex)
+    try : #한국거래소
          PriceDriver = Util.NowPriceDriver(Headless)
-    except:
+    except Exception as ex:
          print("Price Driver Err")
+         print('에러가 발생 했습니다', ex)
     try: # 다음 증권
         KospiImageDriver = Util.Get_KospiGraphDriver(Headless)
-    except:
+    except Exception as ex:
         print("KospiImage Driver Err")
+        print('에러가 발생 했습니다', ex)
     MakeCompanyFile(MakeCompanyList) #기업 리스트 갱신
     DBController = DBHandler.MySqlController(host, ID, PW, DB_name)
 
     label=[]
     while(True):
-           try:
             now = datetime.datetime.now()
             nowDatetime = now.strftime('%Y_%m_%d_%H시%M분%S초'.encode('unicode-escape').decode()).encode().decode('unicode-escape')
             nowDatehour = now.strftime('%Y_%m_%d_%H시%M분'.encode('unicode-escape').decode()).encode().decode('unicode-escape')
-
-            # print("\n\n#########################################")
-            #print("||  [현재 시각] : " + nowDatetime+"  ||")
+            try:
+                NameList, PriceInfo, Fluctuation = Util.get_prices(PriceDriver)
+                Util.PrintPrice(NameList,PriceInfo,Fluctuation)
+            except Exception as ex:
+                print("Price Info Err")
+                print('에러가 발생 했습니다', ex)
+                PriceDriver.quit()
+                PriceDriver = Util.NowPriceDriver(Headless)
+                NameList, PriceInfo, Fluctuation = Util.get_prices(PriceDriver)
             # print("========================================")
-            NameList, PriceInfo, Fluctuation = GetPriceInfo(PriceDriver)
-            # print("========================================")
-            headlines, news_info, Text,NewsUrl,CompanyFromNews = GetNewsInfo(NewsDriver) #뉴스에서 기업 추출
-            print("News Updated...")
-            Util.Write_News(headlines, CompanyFromNews,nowDatehour)  # 기업별 뉴스 자료 Writing
-            Util.GetKospiGraph(KospiImageDriver, PriceInfo, Fluctuation) # Kospi, Kosdaq 그래프 이미지 저장
-            print("Get Kospi Graph")
-            label = predict.classification(headlines,model)
-            print("Get labels")
-            DBController.UpdateNews(CompanyFromNews, headlines, Text, NewsUrl, news_info,label) #최신 20개 기사 DB저장
-            DBController.InsertNewsHistory(CompanyFromNews, headlines, Text, NewsUrl, news_info,nowDatehour)
-            # ==== 30초마다 반복 수행
+            try:
+                headlines, news_info, Text,NewsUrl,CompanyFromNews = GetNewsInfo(NewsDriver) #뉴스에서 기업 추출
+                print("News Updated...")
+            except Exception as ex:
+                print("News Update Err")
+                NewsDriver.quit()
+                NewsDriver = Util.News_get_driver(Headless)
+                print('에러가 발생 했습니다', ex)
+            try:
+                Util.Write_News(headlines, CompanyFromNews, nowDatehour)  # 기업별 뉴스 자료 Writing
+            except Exception as ex:
+                print("News Write Err")
+                CompanyList = Util.GetCompanyList()  # 코스피 상장 기업 업로드
+            try:
+                Util.GetKospiGraph(KospiImageDriver, PriceInfo, Fluctuation) # Kospi, Kosdaq 그래프 이미지 저장
+                print("Get Kospi Graph")
+            except Exception as ex:
+                KospiImageDriver.quit()
+                KospiImageDriver = Util.Get_KospiGraphDriver(Headless)
+                print("Graph Err")
+                print('에러가 발생 했습니다', ex)
+            try:
+                label = predict.classification(headlines, model)
+                print("Get labels")
+                DBController.UpdateNews(CompanyFromNews, headlines, Text, NewsUrl, news_info, label)  # 최신 20개 기사 DB저장
+                DBController.InsertNewsHistory(CompanyFromNews, headlines, Text, NewsUrl, news_info, nowDatehour)
+                print("DB Commit : News Updated, News History Inserted")
+            except Exception as ex:
+                print("Label Err")
+                MakeCompanyFile(MakeCompanyList)  # 기업 리스트 갱신
+                DBController = DBHandler.MySqlController(host, ID, PW, DB_name)
+                print('에러가 발생 했습니다', ex)
             time.sleep(30)
             NewsDriver.refresh()
             PriceDriver.refresh()
             KospiImageDriver.refresh()
             print("DONE")
-           except:
-               NewsDriver.quit()
-               PriceDriver.quit()
-               KospiImageDriver.quit()
-               CompanyList = Util.GetCompanyList()  # 코스피 상장 기업 업로드
-               NewsDriver = Util.News_get_driver(Headless)
-               PriceDriver = Util.NowPriceDriver(Headless)
-               KospiImageDriver = Util.Get_KospiGraphDriver(Headless)
-               print("Web driver is reconstructed")
-               MakeCompanyFile(MakeCompanyList)  # 기업 리스트 갱신
-               DBController = DBHandler.MySqlController(host, ID, PW, DB_name)
-               print("Reconstructing...")
-               pass
